b4459b3b42d3b9a8fb9a2e99c84934969aba752e
Michael Leue <domus@posteo.de>
2018-08-05 20:08:59 +0200

    adding a crude --ls flag to output the list of available commands to query

--- min/cli/cli.py
+++ min/cli/cli.py
@@ -3,11 +3,15 @@ import fire
 from .read_docs import read_docs_filenames, read_docs_file
 
 
-def min(command):
+def min(command, ls=False):
   """Run min cli."""
   docs_path, filenames = read_docs_filenames()
   commands = {filename.split('.')[0]: os.path.join(docs_path, filename)
               for filename in filenames}
+
+  if ls:
+    print(list(commands.keys()))
+    return
 
   try:
     print(read_docs_file(commands[command]))
