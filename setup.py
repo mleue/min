cd57549f09c8fd02d7943678f4f477c8c32826e4
Michael Leue <domus@posteo.de>
2018-08-09 08:36:37 +0200

    add LICENSE and add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+  long_description = fh.read()
+
 setup(
-    name='min',
-    version='0.0.1',
-    description='minimal manpages.',
-    url='',
-    author='Michael Leue',
-    author_email='michael@mleue.com',
-    license='',
-    classifiers=[
-        'Development Status :: 2 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Documentation',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    ],
-    entry_points={
-      'console_scripts': ['min=min.cli.cli:min_cli'],
-    },
-    keywords='manpage minimal man',
-    packages=find_packages(exclude=['docs']),
+  name='min',
+  version='0.0.1',
+  description='minimal manpages.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='',
+  author='Michael Leue',
+  author_email='michael@mleue.com',
+  license='',
+  classifiers=[
+      'Development Status :: 2 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Documentation',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.6',
+  ],
+  entry_points={
+    'console_scripts': ['min=min.cli.cli:min_cli'],
+  },
+  keywords='manpage minimal man',
+  packages=find_packages(exclude=['docs']),
 )
