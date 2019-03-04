776e3378f6ef26acf59af3e6093721eab61cc53f
Michael Leue <domus@posteo.de>
2019-03-04 15:35:13 +0100

    bump version number, also reformat setup.py using python black

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding='utf-8') as fh:
-  long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-  name='miniman',
-  version='0.0.4',
-  description='minimal manpages.',
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  url='',
-  author='Michael Leue',
-  author_email='michael@mleue.com',
-  license='',
-  classifiers=[
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Documentation',
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-  ],
-  entry_points={
-    'console_scripts': ['min=min.cli.cli:min_cli'],
-  },
-  keywords='manpage minimal man',
-  packages=find_packages(),
-  package_data={'min': ['docs/*/*']},
+    name="miniman",
+    version="0.0.5",
+    description="minimal manpages.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    author="Michael Leue",
+    author_email="michael@mleue.com",
+    license="",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Documentation",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": ["min=min.cli.cli:min_cli"]},
+    keywords="manpage minimal man",
+    packages=find_packages(),
+    package_data={"min": ["docs/*/*"]},
 )
