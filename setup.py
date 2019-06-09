b7875c4e7005e18c3226f1b84a303289fa9d7d84
Michael Leue <domus@posteo.de>
2019-06-09 13:26:20 +0200

    add usermod command

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="miniman",
-    version="0.0.7",
+    version="0.0.8",
     description="minimal manpages.",
     long_description=long_description,
     long_description_content_type="text/markdown",
